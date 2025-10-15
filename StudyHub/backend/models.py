40254b5018b227a3b0b9ce20812d79e1c00ee4e5
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-10-15 10:31:57 +0300

    Update models.py

--- StudyHub/backend/models.py
+++ StudyHub/backend/models.py
@@ -8,3 +8,19 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+
+# -----------------------------
+# (Challenge Table)
+# -----------------------------
+
+class Challenge(Base):
+    __tablename__ = "challenges"
+
+    id = Column(Integer, primary_key=True, index=True)  # رقم التحدي (أساسي وأوتوماتيكي)
+    title = Column(String, nullable=False)              # عنوان التحدي (إجباري)
+    description = Column(Text, nullable=True)           # وصف التحدي (اختياري)
+    level = Column(String, nullable=True)               # مستوى التحدي (Easy / Medium / Hard)
+    creator_name = Column(String, nullable=False)       # اسم الشخص الذي أنشأ التحدي (مؤقتًا بدون user_id)
+    start_date = Column(String, nullable=True)          # تاريخ بداية التحدي
+    end_date = Column(String, nullable=True)            # تاريخ نهاية التحدي
+    participants = Column(Integer, default=0)           # عدد المشاركين في التحدي (يبدأ من 0)
