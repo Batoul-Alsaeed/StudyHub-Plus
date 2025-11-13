a72e9ac60749c349a9a4b94667a3c2998ec05760
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-11-13 11:59:26 +0300

    fix

--- StudyHub/backend/database.py
+++ StudyHub/backend/database.py
@@ -4,25 +4,18 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = "postgresql://postgres:B4709903atoul@db.guyoesmoykuioothbewe.supabase.co:5432/postgres"
-
+#engine = create_engine(
+#    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+#)
 engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
-
-#engine = create_engine(
-#    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-#)
-#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#Base = declarative_base()
-
-#def get_db():
-#    db = SessionLocal()
-#    try:
-#        yield db
-#    finally:
-#        db.close()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
